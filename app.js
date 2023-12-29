b069c4dd68a9c7b8c30168743bc1986864f4541d
VeselovTimofey <vestimofey@mail.ru>
2023-12-30 01:32:23 +0300

    feat: create cards route

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '658ef7d49f58e88a2681a306',
+  };
+
+  next();
+});
+
 app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/cards.js
@@ -0,0 +1,24 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+const createCard = (req, res) => {
+  const {name, link} = req.body;
+  const owner = req.user._id;
+
+  Card.create({name, link, owner})
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndDelete(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+module.exports = { getCards, createCard, deleteCard };
