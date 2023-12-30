9a5abf4a7a1fab36b9ea677adaaf57584ee73643
VeselovTimofey <vestimofey@mail.ru>
2023-12-30 22:43:35 +0300

    refactor: change body-parser to express

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
